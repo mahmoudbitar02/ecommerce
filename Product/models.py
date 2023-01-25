0f11e8f7b60f8ef2eb313ed2ce2842da8ace9482
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-25 23:44:08 +0100

    add part of detail_page

--- Product/models.py
+++ Product/models.py
@@ -3,6 +3,7 @@ from taggit.managers import TaggableManager
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -25,6 +26,10 @@ class Product(models.Model):
     slug = models.SlugField(null=True,blank=True)
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
     
     
 class ProductImages(models.Model):
