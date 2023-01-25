7705db7344a60f3a5258e0f9c90e1b67a7a8532c
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-25 22:40:57 +0100

    add base.html

--- project/urls.py
+++ project/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
+from django.urls import include, path
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('Product.urls', namespace='products')),
+    path('Product/', include('Product.urls', namespace='Products')),
 ]
 
 if settings.DEBUG:
