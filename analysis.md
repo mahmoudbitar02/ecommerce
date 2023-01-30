2d5279f16eae66a9f8cd62c4c90c07c2eb33bf99
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-30 21:34:54 +0100

    add order & orderdetail model

---
+++ analysis.md
@@ -0,0 +1,16 @@
+orders:
+    - id 
+    - created_date
+    - Delevery_date
+    - subtotal
+    - delivery_fee
+    - delevery_location
+    - user
+    - order_status [Recieved-Processed-Shipped-Delivered]
+    - total
+
+
+order_detail:
+    - product
+    - quantity
+    - total
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from Product.models import Product
 
 # Create your models here.
+
+ORDER_STATUS= (
+    ('Resieved','Resieved'),
+    ('Processed','Processed'),
+    ('Shipped','Shipped'),
+    ('Delivered','Delivered'),
+)
+
+class Order(models.Model):
+    order_code = models.CharField(max_length=10)
+    user=''
+    order_status=models.CharField(max_length=15, choices=ORDER_STATUS, default='Resieved')
+    delevery_date=models.DateTimeField(null= True, blank=True)
+    order_date=models.DateTimeField(default= timezone.now)
+
+
+
+class Order_Detail(models.Model):
+    Order=models.ForeignKey(Order, related_name='order_detail', on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,related_name='order_product', on_delete=models.SET_NULL, null=True, blank=True)
